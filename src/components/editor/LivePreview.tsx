d8fd119652ef049eae67cf836b62b62c7c1d1d5c
Lewis Ng'ang'a <flammylewis4@gmail.com>
2025-01-09 12:10:13 +0300

    Updated readme with version 2 of the Editor Image

--- src/components/editor/LivePreview.tsx
+++ src/components/editor/LivePreview.tsx
@@ -1,4 +1,3 @@
-// LivePreview.tsx
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../store/store";
@@ -7,11 +6,13 @@ import Base from "../../Base";
 import { HiArrowLeft } from "react-icons/hi";
 import { IoDesktopOutline } from "react-icons/io5";
 import { MdTabletMac } from "react-icons/md";
-import { FaMobileAlt } from "react-icons/fa";
-import { FaFileExport } from "react-icons/fa"; // Import export icon
-import ExportModal from "./ExportModal"; // Import the modal component
+import { FaMobileAlt, FaFileExport } from "react-icons/fa"; // Import export icon
+import { BsFiletypeJson } from "react-icons/bs"; // View JSON icon
 
-const LivePreview = () => {
+import ExportModal from "./ExportModal"; // Import the modal component
+import MonitorTemplateModal from "./MonitorTemplateModal";
+
+const LivePreview: React.FC = () => {
   const dispatch = useDispatch();
   const selectedTemplateId = useSelector(
     (state: RootState) => state.template.selectedTemplateId
@@ -21,6 +22,7 @@ const LivePreview = () => {
     "desktop"
   );
   const [isExportModalOpen, setExportModalOpen] = useState(false);
+  const [isMonitorModalOpen, setMonitorModalOpen] = useState(false);
 
   const modeStyles = {
     desktop: "w-full h-full",
@@ -28,14 +30,18 @@ const LivePreview = () => {
     mobile: "w-[375px] h-[667px]",
   };
 
-  const handleExportClick = () => {
-    setExportModalOpen(true);
+  const handleModalToggle = (modalType: "export" | "monitor") => {
+    if (modalType === "export") {
+      setExportModalOpen(!isExportModalOpen);
+    } else {
+      setMonitorModalOpen(!isMonitorModalOpen);
+    }
   };
 
   return (
     <div
       className={`h-full flex flex-col ${
-        isExportModalOpen ? "pointer-events-none" : ""
+        isExportModalOpen || isMonitorModalOpen ? "pointer-events-none" : ""
       }`}
     >
       <div className="flex items-center bg-[#ededed] rounded-md gap-4 border-b border-gray-300 p-2">
@@ -46,52 +52,44 @@ const LivePreview = () => {
           <HiArrowLeft className="text-xl text-gray-700" />
         </button>
         <div className="flex items-center gap-4">
+          {["desktop", "tablet", "mobile"].map((mode) => (
+            <button
+              key={mode}
+              onClick={() =>
+                setDeviceMode(mode as "desktop" | "tablet" | "mobile")
+              }
+              className={`p-2 rounded-full transition-colors ${
+                deviceMode === mode
+                  ? "bg-[#1f2937] text-white"
+                  : "bg-[#d1d1d1] text-gray-700 hover:bg-[#c1c1c1]"
+              }`}
+            >
+              {mode === "desktop" && <IoDesktopOutline className="text-xl" />}
+              {mode === "tablet" && <MdTabletMac className="text-xl" />}
+              {mode === "mobile" && <FaMobileAlt className="text-xl" />}
+            </button>
+          ))}
           <button
-            onClick={() => setDeviceMode("desktop")}
-            className={`p-2 rounded-full transition-colors ${
-              deviceMode === "desktop"
-                ? "bg-[#1f2937] text-white"
-                : "bg-[#d1d1d1] text-gray-700 hover:bg-[#c1c1c1]"
-            }`}
-          >
-            <IoDesktopOutline className="text-xl" />
-          </button>
-          <button
-            onClick={() => setDeviceMode("tablet")}
-            className={`p-2 rounded-full transition-colors ${
-              deviceMode === "tablet"
-                ? "bg-[#1f2937] text-white"
-                : "bg-[#d1d1d1] text-gray-700 hover:bg-[#c1c1c1]"
-            }`}
-          >
-            <MdTabletMac className="text-xl" />
-          </button>
-          <button
-            onClick={() => setDeviceMode("mobile")}
-            className={`p-2 rounded-full transition-colors ${
-              deviceMode === "mobile"
-                ? "bg-[#1f2937] text-white"
-                : "bg-[#d1d1d1] text-gray-700 hover:bg-[#c1c1c1]"
-            }`}
-          >
-            <FaMobileAlt className="text-xl" />
-          </button>
-          <button
-            onClick={handleExportClick}
+            onClick={() => handleModalToggle("export")}
             className="p-2 bg-[#d1d1d1] rounded-full hover:bg-[#c1c1c1] transition-colors"
           >
             <FaFileExport className="text-xl text-gray-700" />
+          </button>
+          <button
+            onClick={() => handleModalToggle("monitor")}
+            className="p-2 bg-[#d1d1d1] rounded-full hover:bg-[#c1c1c1] transition-colors"
+          >
+            <BsFiletypeJson className="text-xl text-gray-700" />
           </button>
         </div>
       </div>
       <div
         className={`flex justify-center items-center bg-gray-100 mt-3 mx-auto overflow-hidden ${
           modeStyles[deviceMode]
-        } ${isExportModalOpen ? "pointer-events-none" : ""}`}
-        style={{
-          overflow: "hidden",
-          position: "relative",
-        }}
+        } ${
+          isExportModalOpen || isMonitorModalOpen ? "pointer-events-none" : ""
+        }`}
+        style={{ overflow: "hidden", position: "relative" }}
       >
         {selectedTemplateId ? (
           <Base id={selectedTemplateId} />
@@ -101,6 +99,9 @@ const LivePreview = () => {
       </div>
       {isExportModalOpen && (
         <ExportModal onClose={() => setExportModalOpen(false)} />
+      )}
+      {isMonitorModalOpen && (
+        <MonitorTemplateModal onClose={() => setMonitorModalOpen(false)} />
       )}
     </div>
   );
