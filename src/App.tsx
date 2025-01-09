3405091275ae6ddd81ceabb499b647ad28aa0408
Lewis Ng'ang'a <flammylewis4@gmail.com>
2025-01-09 15:03:31 +0300

    Fixed old v1 editor implementation with new core.

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./modules/templateRegistry"; // Register Templates in the Registry
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Base from "./Base";
-import ContentEditor from "./components/samples/ContentEditor";
+import ContentEditor from "./components/admin/TemplateConfig";
 import Editor from "./components/editor/Editor";
 function App() {
   return (
